64b66f8486a3a0ff05b4c80a004b358a8912498a
darenjacobs <darenjacobs@msn.com>
2018-08-28 14:51:10 -0400

    report loop

--- report.sh
+++ report.sh
@@ -4,28 +4,16 @@ export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 
 pip install boto3 PrettyTable PTable termcolor --upgrade --user
 
-echo "#################"
-echo "US EAST Region 1"
-export AWS_DEFAULT_REGION=us-east-1
-python ./ami-list/ami_list.py -r us-east-1
-python ./ami-list/ec2-state.py
-echo "#################"
-echo ""
 
-echo "#################"
-echo "US East Region 2"
-export AWS_DEFAULT_REGION=us-east-2
-python ./ami-list/ami_list.py -r us-east-2
-python ./ami-list/ec2-state.py
-echo "#################"
-echo ""
-
-echo "#################"
-echo "US West Region 2"
-export AWS_DEFAULT_REGION=us-west-2
-python ./ami-list/ami_list.py -r us-west-2
-python ./ami-list/ec2-state.py
-echo "#################"
-echo ""
+for region in ["us-east-1", "us-east-2", "us-west-2"]
+do
+  echo "#################"
+  echo "REGION: ${region}"
+  export AWS_DEFAULT_REGION=${region}
+  python ./ami-list/ami_list.py -r ${region}
+  python ./ami-list/ec2-state.py
+  echo "#################"
+  echo ""
+done
 
 python ./ami-list/ami_list.py -r us-west-1
