502f7dab9f51c7753b78b993ca3411324bd48333
darenjacobs <darenjacobs@msn.com>
2018-08-28 10:35:30 -0400

    color based on running state

--- ec2-state.py
+++ ec2-state.py
@@ -5,8 +5,15 @@ from termcolor import colored
 
 ec2 = boto3.resource('ec2')
 for i in ec2.instances.all():
-    print("ID: {0}\tState: {1}\tAMI ID: {2}".format(
-        colored(i.id, 'cyan'),
-        colored(i.state['Name'], 'green'),
-        colored(i.image_id, 'yellow')
-    ))
+    if i.state['Name'] == "running":
+        print("ID: {0}\tState: {1}\tAMI ID: {2}".format(
+            colored(i.id, 'cyan'),
+            colored(i.state['Name'], 'green'),
+            colored(i.image_id, 'yellow')
+        ))
+    else:
+        print("ID: {0}\tState: {1}\tAMI ID: {2}".format(
+            colored(i.id, 'cyan'),
+            colored(i.state['Name'], 'red'),
+            colored(i.image_id, 'yellow')
+        ))
