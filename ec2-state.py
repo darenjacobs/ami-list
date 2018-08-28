2a6d981ba627df80fd43340b16840fa25d2aff45
darenjacobs <darenjacobs@msn.com>
2018-08-28 14:44:09 -0400

    print total

--- ec2-state.py
+++ ec2-state.py
@@ -2,10 +2,14 @@
 
 import boto3
 from termcolor import colored
+from collections import Counter
+
+c = Counter(running=0, stopped=0)
 
 ec2 = boto3.resource('ec2')
 for i in ec2.instances.all():
     if i.state['Name'] == "running":
+        c['running'] += 1
         print("ID: {0}\tState: {1}\tAMI ID: {2}".format(
             colored(i.id, 'cyan'),
             colored(i.state['Name'], 'green'),
@@ -14,8 +18,12 @@ for i in ec2.instances.all():
 
 for i in ec2.instances.all():
     if i.state['Name'] == 'stopped':
+        c['stopped'] += 1
         print("ID: {0}\tState: {1}\tAMI ID: {2}".format(
             colored(i.id, 'cyan'),
             colored(i.state['Name'], 'red'),
             colored(i.image_id, 'yellow')
         ))
+
+print("\nRunning: {0} Stopped: {1}".format(colored(c['running'], 'green'),
+                                           colored(c['stopped'], 'red')))
