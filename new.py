3fce775c1aa9f63ea76794624857e310a5513501
darenjacobs <daren.jacobs@kaltura.com>
2018-08-28 21:00:38 -0400

    closer to new

--- new.py
+++ new.py
@@ -3,13 +3,18 @@
 import boto3
 from termcolor import colored
 from collections import Counter
+from operator import itemgetter, attrgetter
 
 c = Counter(running=0, stopped=0)
-instance = {}
+instances = {}
+my_dict = []
 
 ec2 = boto3.resource('ec2')
 for i in ec2.instances.all():
-    instances = {'id': i.id, 'state': i.state, 'image': i.image_id}
+    # print(i.id)
+    instances = {'id': i.id, 'state': i.state['Name'], 'image': i.image_id}
+    my_dict.append(instances)
+
     if i.state['Name'] == "running":
         c['running'] += 1
     elif i.state['Name'] == "stopped":
@@ -17,13 +22,10 @@ for i in ec2.instances.all():
     else:
         print('ERROR: running state unknown')
 
+sorted_list = sorted(my_dict, key=itemgetter('state'))
 
-my_list = sorted(instances, key=lambda key: instances[key])
-
-for id, state in instances.items():
-    # print ("ID: %s, State: %s Image: %s" % id, state, instances['image'])
-    print (id, state)
-
+# my_list = sorted(instances, key=lambda key: instances[key])
+print(sorted_list)
 
 if c['running'] or c['stopped']:
     print("\nRunning: {0} Stopped: {1}".format(colored(c['running'], 'green'),
